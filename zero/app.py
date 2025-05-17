10a23cee8e241224179371ef9f8d26aa8ddfc0c4
Junior <eecfredes@gmail.com>
2025-05-17 20:53:13 -0300

    :rocket:Status code, response, done

--- zero/app.py
+++ zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read():
-    return {'msg': 'Test API'}
+    return {"msg": "Test API"}
