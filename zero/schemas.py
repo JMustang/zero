954f0a32d8022a8c355900dfc3c55f7a29a501d4
Junior <eecfredes@gmail.com>
2025-05-16 22:02:15 -0300

    :rocket:User schema

---
+++ zero/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr
+
+
+class Message(BaseModel):
+    Message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
