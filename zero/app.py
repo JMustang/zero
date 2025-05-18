55b5968c3dac37033d91780dea3f7243f7367bba
Junior <eecfredes@gmail.com>
2025-05-17 21:31:23 -0300

    :rocket:Create user, done

--- zero/app.py
+++ zero/app.py
@@ -2,11 +2,16 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from zero.schemas import Message
+from zero.schemas import Message, UserSchema, UserPublicSchema
 
-app = FastAPI()
+app = FastAPI(title="Test for Zero API", description="FastAPI", version="0.1.0")
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read():
     return {"msg": "Test API"}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
+def create_user(user: UserSchema):
+    return user
