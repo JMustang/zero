52a1fe2d00d5a7c0af79c752c13fe41faa827010
Junior <eecfredes@gmail.com>
2025-05-18 20:26:35 -0300

    :rotating_light:Format app

--- zero/app.py
+++ zero/app.py
@@ -2,26 +2,25 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from zero.schemas import Message, UserSchema, UserPublicSchema, UserDB
+from zero.schemas import Message, UserDB, UserPublicSchema, UserSchema
 
 # Banco local para testes
 database = []
 
-app = FastAPI(title="Test for Zero API", description="FastAPI", version="0.1.0")
+app = FastAPI(
+    title='Test for Zero API',
+    description='Treinamento com python e FastAPI. Ministrado por dunossauro',
+    version='0.1.0',
+)
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read():
-    return {"msg": "Test API"}
+    return {'msg': 'Test API'}
 
 
-@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        username=user.username,
-        email=user.email,
-        password=user.password,
-        id=len(database) + 1,
-    )
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
     return user_with_id
