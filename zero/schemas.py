094c5680d8ea51175008f4071ea2bfcc5b5a2bfb
Junior <eecfredes@gmail.com>
2025-05-17 21:21:47 -0300

    :rocket:User public schema, done

--- zero/schemas.py
+++ zero/schemas.py
@@ -9,3 +9,11 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class UserPublicSchema(BaseModel):
+    username: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
