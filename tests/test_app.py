6ed382756e7868ee71a69d7e773d9bf333637b16
Junior <eecfredes@gmail.com>
2025-05-19 21:17:33 -0300

    :rocket:Simplifying the test client

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,34 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from zero.app import app
 
 
-def test_app():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_app(client):
     # Act
-    response = client.get("/")
+    response = client.get('/')
 
     # Assert
-    assert response.json() == {"message": "Test API"}
+    assert response.json() == {'message': 'Test API'}
     assert response.status_code == HTTPStatus.OK
 
 
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
     response = client.post(
-        "/users/",
-        json={"username": "alice", "email": "alice@exemple.com", "password": "secret"},
+        '/users/',
+        json={'username': 'alice', 'email': 'alice@exemple.com', 'password': 'secret'},
     )
 
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        "id": 1,
-        "username": "alice",
-        "email": "alice@exemple.com",
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@exemple.com',
     }
