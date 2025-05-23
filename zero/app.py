c3f0f0f4f39b2893c175920d50cbcfb389f08799
Junior <eecfredes@gmail.com>
2025-05-22 22:13:46 -0300

    :rotating_light:lint update

--- zero/app.py
+++ zero/app.py
@@ -39,3 +39,10 @@ def update_user(user_id: int, user: UserSchema):
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='❌ User not found!')
     database[user_id - 1] = user_with_id
     return user_with_id
+
+
+@app.delete('/users/{user_id}', status_code=HTTPStatus.NO_CONTENT)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='❌ User not found!')
+    del database[user_id - 1]
