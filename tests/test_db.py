d95a8414ded3c3069606aa018f04cefd27634cd8
Junior <eecfredes@gmail.com>
2025-05-25 21:58:35 -0300

    :rocket:Using scalar on model user test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,13 @@
+from sqlalchemy import select
+
 from zero.models import User
 
 
 def test_create_user(session):
-    user = User(username='testuser', email='test@example.com', password='secret')
+    new_user = User(username='testuser', email='test@example.com', password='secret')
 
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'testuser'))
     assert user.username == 'testuser'
