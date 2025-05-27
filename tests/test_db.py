d04901c1e904f99adafa53baa95af42f4123dc71
Junior <eecfredes@gmail.com>
2025-05-26 22:02:50 -0300

    :white_check_mark:Using mock db time on test create user

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,17 +5,19 @@ from sqlalchemy import select
 from zero.models import User
 
 
-def test_create_user(session):
-    new_user = User(username='testuser', email='test@example.com', password='secret')
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='testuser', email='test@example.com', password='secret')
 
-    session.add(new_user)
-    session.commit()
+        session.add(new_user)
+        session.commit()
 
-    user = session.scalar(select(User).where(User.username == 'testuser'))
+        user = session.scalar(select(User).where(User.username == 'testuser'))
+
     assert asdict(user) == {
         'id': 1,
         'username': 'testuser',
         'email': 'test@example.com',
         'password': 'secret',
-        'created_at': user.created_at,
+        'created_at': time,
     }
