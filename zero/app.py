dd13f233f8d42b2f9b2da65032d8689369408acc
Junior <eecfredes@gmail.com>
2025-05-31 21:50:31 -0300

    :rocket:Connecting the API to the database

--- zero/app.py
+++ zero/app.py
@@ -1,8 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
 
+from zero.models import User
 from zero.schemas import Message, UserDB, UserList, UserPublicSchema, UserSchema
+from zero.settings import Settings
 
 # Banco local para testes
 database = []
@@ -21,9 +25,29 @@ def read():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
-    database.append(user_with_id)
-    return user_with_id
+    engine = create_engine(
+        Settings().DATABASE_URL,
+    )
+    session = Session(bind=engine)
+    db_user = session.scalar(
+        select(User).where((User.username == user.username) | (User.email == user.email))
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='❌ Username already exists!'
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='❌ Email already exists!'
+            )
+    db_user = User(**user.model_dump())
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
