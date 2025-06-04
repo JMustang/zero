6241636f6d5f2fb8752964165739501998dc166b
Junior <eecfredes@gmail.com>
2025-06-03 22:45:18 -0300

    :white_check_mark:client fixture with depend

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,18 +7,26 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from zero.app import app
+from zero.database import get_session
 from zero.models import table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def session():
     engine = create_engine(
-        'sqlite:///:memory:', connect_args={'check_same_thread': False}
+        "sqlite:///:memory:", connect_args={"check_same_thread": False}
     )
     table_registry.metadata.create_all(bind=engine)
     with Session(bind=engine) as session:
@@ -30,14 +38,14 @@ def session():
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2025, 5, 5)):
     def fake_time_hook(mapper, connection, target):
-        if hasattr(target, 'created_at'):
+        if hasattr(target, "created_at"):
             target.created_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    event.listen(model, "before_insert", fake_time_hook)
 
     yield time
 
-    event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, "before_insert", fake_time_hook)
 
 
 @pytest.fixture
