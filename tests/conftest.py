b1a96f60495accb8ab9f563581968520a930ecb3
Junior <eecfredes@gmail.com>
2025-06-06 22:02:39 -0300

    :rocket:New user fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 
 from zero.app import app
 from zero.database import get_session
-from zero.models import table_registry
+from zero.models import User, table_registry
 
 
 @pytest.fixture
@@ -23,11 +23,11 @@ def client(session):
     app.dependency_overrides.clear()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def session():
     engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
         poolclass=StaticPool,
     )
     table_registry.metadata.create_all(bind=engine)
@@ -40,16 +40,28 @@ def session():
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2025, 5, 5)):
     def fake_time_hook(mapper, connection, target):
-        if hasattr(target, 'created_at'):
+        if hasattr(target, "created_at"):
             target.created_at = time
+        if hasattr(target, "updated_at"):
+            target.updated_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    event.listen(model, "before_insert", fake_time_hook)
 
     yield time
 
-    event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, "before_insert", fake_time_hook)
 
 
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session: Session):
+    user = User(username="teste", email="teste@test.com", password="secret")
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
