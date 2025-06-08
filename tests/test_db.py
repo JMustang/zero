2ff6f9d772911d090090a3b27222fa6230195540
Junior <eecfredes@gmail.com>
2025-06-08 20:59:43 -0300

    :rocket: Adding updated_at on test_db

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,17 +7,20 @@ from zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(username='testuser', email='test@example.com', password='secret')
+        new_user = User(
+            username="testuser", email="test@example.com", password="secret"
+        )
 
         session.add(new_user)
         session.commit()
 
-        user = session.scalar(select(User).where(User.username == 'testuser'))
+        user = session.scalar(select(User).where(User.username == "testuser"))
 
     assert asdict(user) == {
-        'id': 1,
-        'username': 'testuser',
-        'email': 'test@example.com',
-        'password': 'secret',
-        'created_at': time,
+        "id": 1,
+        "username": "testuser",
+        "email": "test@example.com",
+        "password": "secret",
+        "created_at": time,
+        "updated_at": time,
     }
