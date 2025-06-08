924b94b323675e4533e1de2d8988e6f1a917f1b8
Junior <eecfredes@gmail.com>
2025-06-07 22:39:07 -0300

    :rocket:model config

--- zero/schemas.py
+++ zero/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class Message(BaseModel):
@@ -12,9 +12,10 @@ class UserSchema(BaseModel):
 
 
 class UserPublicSchema(BaseModel):
+    id: int
     username: str
     email: EmailStr
-    id: int
+    model_config = ConfigDict(from_attributes=True, extra="forbid")
 
 
 class UserDB(UserSchema):
