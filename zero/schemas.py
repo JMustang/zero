f6c218e80cca8f3f8cbd7e084bbd30d941e4104b
Junior <eecfredes@gmail.com>
2025-06-14 21:31:07 -0300

    :art:Code format

--- zero/schemas.py
+++ zero/schemas.py
@@ -15,7 +15,7 @@ class UserPublicSchema(BaseModel):
     id: int
     username: str
     email: EmailStr
-    model_config = ConfigDict(from_attributes=True, extra="forbid")
+    model_config = ConfigDict(from_attributes=True, extra='forbid')
 
 
 class UserList(BaseModel):
