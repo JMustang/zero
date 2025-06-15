00b1fce670f35bb20e979932b3588b8d73d4bd56
Junior <eecfredes@gmail.com>
2025-06-14 21:28:34 -0300

    :pencil2:Fixing typo

--- zero/security.py
+++ zero/security.py
@@ -1,10 +1,11 @@
-from pwdlib import PasswordHash
 from datetime import datetime, timedelta
-from jwt import encode
 from zoneinfo import ZoneInfo
 
-SECRET_KEY = "secret key"
-ALGORITHM = "HS256"
+from jwt import encode
+from pwdlib import PasswordHash
+
+SECRET_KEY = 'secret key'
+ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = PasswordHash.recommended()
@@ -18,11 +19,11 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_toke(data: dict):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(tz=ZoneInfo("UTC")) + timedelta(
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
     )
-    to_encode.update({"exp": expire})
+    to_encode.update({'exp': expire})
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
