64dc212c825fb30f5e4cba128c8e45e2983d3075
Junior <eecfredes@gmail.com>
2025-06-18 21:38:44 -0300

    :rocket: create function get current user

--- zero/security.py
+++ zero/security.py
@@ -1,14 +1,20 @@
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 from jwt import encode
 from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+
+from zero.database import get_session
 
 SECRET_KEY = 'secret_key'
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
 def get_password_hash(password: str):
@@ -27,3 +33,9 @@ def create_access_token(data: dict):
     to_encode.update({'exp': expire})
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+): ...
