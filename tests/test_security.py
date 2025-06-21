57fe8ba6fd76da7b07432ddeaa6238d4fc4c2410
Junior <eecfredes@gmail.com>
2025-06-21 20:58:49 -0300

    :white_check_mark: test jwt invalid token

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,14 +1,27 @@
+from http import HTTPStatus
+
 from jwt import decode
 
 from zero.security import ALGORITHM, SECRET_KEY, create_access_token
 
 
 def test_jwt():
-    data = {'test': 'test'}
+    data = {"test": "test"}
     token = create_access_token(data)
 
     decoded = decode(token, SECRET_KEY, algorithms=ALGORITHM)
 
-    assert decoded['test'] == 'test'
-    assert 'exp' in decoded
-    assert decoded['exp'] > 0  # Ensure expiration is a positive timestamp
+    assert decoded["test"] == "test"
+    assert "exp" in decoded
+    assert decoded["exp"] > 0  # Ensure expiration is a positive timestamp
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        "/users/1", headers={"Authorization": "Bearer token-invalido"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {
+        "detail": "❌ Could not validate credentials",
+    }
