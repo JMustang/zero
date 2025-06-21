c1326fd3c6f206176e6ef04f9dcbe0d676e80d2e
Junior <eecfredes@gmail.com>
2025-06-21 20:59:14 -0300

    :lipstick:Code format

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,130 +5,130 @@ from zero.schemas import UserPublicSchema
 
 def test_app(client):
     # Act
-    response = client.get("/")
+    response = client.get('/')
 
     # Assert
-    assert response.json() == {"message": "Test API"}
+    assert response.json() == {'message': 'Test API'}
     assert response.status_code == HTTPStatus.OK
 
 
 def test_create_user(client):
     response = client.post(
-        "/users/",
-        json={"username": "alice", "email": "alice@example.com", "password": "secret"},
+        '/users/',
+        json={'username': 'alice', 'email': 'alice@example.com', 'password': 'secret'},
     )
 
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        "id": 1,
-        "username": "alice",
-        "email": "alice@example.com",
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
     }
 
 
 def test_real_users(client, user, token):
     user_schema = UserPublicSchema.model_validate(user).model_dump()
-    response = client.get("/users/", headers={"Authorization": f"Bearer {token}"})
+    response = client.get('/users/', headers={'Authorization': f'Bearer {token}'})
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"users": [user_schema]}
+    assert response.json() == {'users': [user_schema]}
 
 
 def test_get_user_by_id(client, user):
-    response = client.get("/users/1")
+    response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "username": "teste",
-        "email": "teste@test.com",
-        "id": 1,
+        'username': 'teste',
+        'email': 'teste@test.com',
+        'id': 1,
     }
 
 
 def test_update_user(client, user, token):
     response = client.put(
-        "/users/1",
-        headers={"Authorization": f"Bearer {token}"},
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            "username": "bob",
-            "email": "bob@example.com",
-            "password": "secret",
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
         },
     )
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "username": "bob",
-        "email": "bob@example.com",
-        "id": 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
     }
 
 
 def test_update_integrity_error(client, user, token):
     client.post(
-        "/users",
+        '/users',
         json={
-            "username": "fausto",
-            "email": "fausto@exemplo.com",
-            "password": "secret",
+            'username': 'fausto',
+            'email': 'fausto@exemplo.com',
+            'password': 'secret',
         },
     )
 
     resopnse_update = client.put(
-        f"/users/{user.id}",
-        headers={"Authorization": f"Bearer {token}"},
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            "username": "fausto",
-            "email": "bob@exemplo.com",
-            "password": "mynewpassword",
+            'username': 'fausto',
+            'email': 'bob@exemplo.com',
+            'password': 'mynewpassword',
         },
     )
 
     assert resopnse_update.status_code == HTTPStatus.CONFLICT
-    assert resopnse_update.json() == {"detail": "❌ Username of Email already exists!"}
+    assert resopnse_update.json() == {'detail': '❌ Username of Email already exists!'}
 
 
 def test_update_user_not_authorized(client, token):
     response = client.put(
-        "/users/2",
-        headers={"Authorization": f"Bearer {token}"},
+        '/users/2',
+        headers={'Authorization': f'Bearer {token}'},
         json={
-            "username": "bob",
-            "email": "bob@example.com",
-            "password": "secret",
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
         },
     )
     assert response.status_code == HTTPStatus.FORBIDDEN
     assert response.json() == {
-        "detail": "❌ You do not have permission to update this user."
+        'detail': '❌ You do not have permission to update this user.'
     }
 
 
 def test_delete_user(client, user, token):
     response = client.delete(
-        "/users/1",
-        headers={"Authorization": f"Bearer {token}"},
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
     )
     assert response.status_code == HTTPStatus.NO_CONTENT
-    assert response.content == b""
+    assert response.content == b''
 
 
 def test_delete_user_not_found(client, token):
     response = client.delete(
-        "/users/2",
-        headers={"Authorization": f"Bearer {token}"},
+        '/users/2',
+        headers={'Authorization': f'Bearer {token}'},
     )
     assert response.status_code == HTTPStatus.FORBIDDEN
     assert response.json() == {
-        "detail": "❌ You do not have permission to update this user."
+        'detail': '❌ You do not have permission to update this user.'
     }
 
 
 def test_get_token(client, user):
     response = client.post(
-        "/token",
-        data={"username": user.email, "password": user.clean_password},
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
     )
     token = response.json()
 
     assert response.status_code == HTTPStatus.OK
-    assert token["token_type"] == "Bearer"
-    assert "access_token" in token
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
--- zero/app.py
+++ zero/app.py
@@ -17,33 +17,31 @@ from zero.security import (
 )
 
 app = FastAPI(
-    title="Test for Zero API",
-    description="Treinamento com python e FastAPI. Ministrado por dunossauro",
-    version="0.1.0",
+    title='Test for Zero API',
+    description='Treinamento com python e FastAPI. Ministrado por dunossauro',
+    version='0.1.0',
 )
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read():
-    return {"message": "Test API"}
+    return {'message': 'Test API'}
 
 
-@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
-        select(User).where(
-            (User.username == user.username) | (User.email == user.email)
-        )
+        select(User).where((User.username == user.username) | (User.email == user.email))
     )
 
     if db_user:
         if db_user.username == user.username:
             raise HTTPException(
-                status_code=HTTPStatus.CONFLICT, detail="❌ Username already exists!"
+                status_code=HTTPStatus.CONFLICT, detail='❌ Username already exists!'
             )
         elif db_user.email == user.email:
             raise HTTPException(
-                status_code=HTTPStatus.CONFLICT, detail="❌ Email already exists!"
+                status_code=HTTPStatus.CONFLICT, detail='❌ Email already exists!'
             )
 
     db_user = User(
@@ -57,20 +55,20 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
         session.refresh(db_user)
     except IntegrityError as e:
         session.rollback()
-        if "UNIQUE constraint failed" in str(e):
+        if 'UNIQUE constraint failed' in str(e):
             raise HTTPException(
                 status_code=HTTPStatus.CONFLICT,
-                detail="❌ Username or Email already exists!",
+                detail='❌ Username or Email already exists!',
             )
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail="❌ An error occurred while creating the user.",
+            detail='❌ An error occurred while creating the user.',
         )
 
     return db_user
 
 
-@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def list_users(
     skip: int = 0,
     limit: int = 100,
@@ -78,22 +76,20 @@ def list_users(
     current_user=Depends(get_current_user),
 ):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {"users": users}
+    return {'users': users}
 
 
-@app.get("/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublicSchema)
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublicSchema)
 def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
     db_user = session.scalar(select(User).where(User.id == user_id))
 
     if not db_user:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="❌ User not found!"
-        )
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='❌ User not found!')
 
     return db_user
 
 
-@app.put("/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublicSchema)
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublicSchema)
 def update_user(
     user_id: int,
     user: UserSchema,
@@ -103,7 +99,7 @@ def update_user(
     if current_user.id != user_id:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN,
-            detail="❌ You do not have permission to update this user.",
+            detail='❌ You do not have permission to update this user.',
         )
     try:
         current_user.username = user.username
@@ -115,18 +111,18 @@ def update_user(
         return current_user
     except IntegrityError as e:
         session.rollback()
-        if "UNIQUE constraint failed" in str(e):
+        if 'UNIQUE constraint failed' in str(e):
             raise HTTPException(
                 status_code=HTTPStatus.CONFLICT,
-                detail="❌ Username of Email already exists!",
+                detail='❌ Username of Email already exists!',
             )
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail="❌ An error occurred while updating the user.",
+            detail='❌ An error occurred while updating the user.',
         )
 
 
-@app.delete("/users/{user_id}", status_code=HTTPStatus.NO_CONTENT)
+@app.delete('/users/{user_id}', status_code=HTTPStatus.NO_CONTENT)
 def delete_user(
     user_id: int,
     session: Session = Depends(get_session),
@@ -135,15 +131,15 @@ def delete_user(
     if current_user.id != user_id:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN,
-            detail="❌ You do not have permission to update this user.",
+            detail='❌ You do not have permission to update this user.',
         )
 
     session.delete(current_user)
     session.commit()
-    return {"message": "✅ User deleted successfully"}
+    return {'message': '✅ User deleted successfully'}
 
 
-@app.post("/token", response_model=Token)
+@app.post('/token', response_model=Token)
 def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     session: Session = Depends(get_session),
@@ -152,13 +148,13 @@ def login_for_access_token(
     if not user:
         raise HTTPException(
             status_code=HTTPStatus.UNAUTHORIZED,
-            detail="❌ Incorrect email or password",
+            detail='❌ Incorrect email or password',
         )
     if not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=HTTPStatus.UNAUTHORIZED,
-            detail="❌ Incorrect email or password",
+            detail='❌ Incorrect email or password',
         )
 
-    access_token = create_access_token({"sub": user.email})
-    return {"access_token": access_token, "token_type": "Bearer"}
+    access_token = create_access_token({'sub': user.email})
+    return {'access_token': access_token, 'token_type': 'Bearer'}
